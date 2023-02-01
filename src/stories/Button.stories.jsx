59a9a491fcd483c02df9794eb79fe7f2eb08d37d
Gary Fagan <gary.fagan@workbuzz.com>
2023-02-01 09:49:41 +0000

    change

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button Main',
+  label: 'Button New Task',
 };
 
 export const Secondary = Template.bind({});
